42796a18ffb42deb022cafa2966bd78ce319fa1a
Ruben Deambrossi <ruben.deambrossi@foxconn.com>
2018-05-24 14:48:43 -0600

    submit form by ajax

--- Books_MVC/Controllers/AjaxScriptsController.cs
+++ Books_MVC/Controllers/AjaxScriptsController.cs
@@ -11,6 +11,8 @@ namespace Books_MVC.Controllers
         //
         // GET: /AjaxScripts/
 
+        public static List<string> _comments = new List<string>() { "comment1", "comment2", "comment3", "comment4" };
+
         public ActionResult Index()
         {
             return View();
@@ -21,5 +23,34 @@ namespace Books_MVC.Controllers
             return PartialView();
         }
 
+
+        public ActionResult ShowComments()
+        {
+            return View(_comments);
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(string comment)
+        {
+            _comments.Add(comment);
+
+            if (Request.IsAjaxRequest())
+            {
+                /*
+                    if request is ajax then fill a partial view that return an html
+                    that will be added to the <ul> list by javascript
+                 */
+
+                ViewBag.Comment = comment;
+                return PartialView();
+            }
+
+            /* 
+                if request is not ajax then redirect to the ShowComment 
+                action after adding the new comment to the list
+             */
+
+            return RedirectToAction("ShowComments");
+        }
     }
 }
---
+++ Books_MVC/Scripts/js/add-comments.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    $("#commentForm").submit(function (event) {
+        event.preventDefault();
+
+        var data = $(this).serialize();
+        var url = $(this).attr('action');
+
+        $.post(url, data, function (response) {
+            $("#comments").append(response);
+        });
+
+    });
+    
+});
