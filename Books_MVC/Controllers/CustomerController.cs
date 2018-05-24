3da5fffe55b258f4aafed0810ee296be57334814
Ruben Deambrossi <ruben.deambrossi@foxconn.com>
2018-05-24 10:03:01 -0600

    Different Types of Models
    * Domain Model: Representation of Data Table from Data Base
    * View Model: Model to send to the View, when two or more Domain models
    need to be merge into onle one view then create the View Model to just
    fill it with the information and send to the view.
    * Input Model: Model with the user input information to sent from view
    to controller

---
+++ Books_MVC/Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Books_MVC.Models;
+
+namespace Books_MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+
+        public List<Customer> CustomerOrders = new List<Customer>()
+        {
+            new Customer(){
+                Id=1, 
+                Name="alex", 
+                ServiceLevel="normal", 
+                Orders = new List<Order>() { 
+                    new Order(){
+                        Id=1,
+                        OrderTime=DateTime.Now
+                    }
+                }
+            },
+            new Customer(){
+                Id=2, 
+                Name="ruben", 
+                ServiceLevel="normal", 
+                Orders = new List<Order>() { 
+                    new Order(){
+                        Id=2,
+                        OrderTime=DateTime.Now.AddMinutes(1)
+                    },
+                    new Order(){
+                        Id=3,
+                        OrderTime=DateTime.Now.AddMinutes(2)
+                    },
+                    new Order(){
+                        Id=3,
+                        OrderTime=DateTime.Now.AddMinutes(3)
+                    },
+                }
+            }
+        };
+
+
+        public ActionResult Index()
+        {
+            List<CustomerSummary> summary = new List<CustomerSummary>();
+
+            foreach (Customer c in CustomerOrders)
+            {
+                List<Order> cus_order = c.Orders.OrderByDescending(i => i.OrderTime).ToList();
+
+                string cus_name = c.Name;
+                string cus_service = c.ServiceLevel;
+
+                int cus_or_count = cus_order.Count;
+                string cus_resent_ord = "";
+                int cus_or_id = 0;
+
+                if (cus_order.Count > 0)
+                {
+                    cus_resent_ord = cus_order[0].OrderTime.ToString();
+                    cus_or_id = cus_order[0].Id;
+                }
+
+
+                summary.Add(new CustomerSummary()
+                {
+                    Name = cus_name,
+                    ServiceLevel = cus_service,
+                    MostRecentOrderDate = cus_resent_ord,
+                    OrderCount = cus_or_count.ToString(),
+                    Input = new CustomerSummary.CustomerSumaryInput()
+                    {
+                        Number = 0,
+                        Active = false
+                    }
+                });
+            }
+
+
+            return View("IndexCustomers", summary);
+        }
+
+        public ActionResult ChangeStatus(List<CustomerSummary> input)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Books_MVC/Models/CustomerSummary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books_MVC.Models
+{
+    /* [view model] */
+    public class CustomerSummary
+    { 
+        public string Name { get; set; }
+        public string ServiceLevel { get; set; }
+        public string OrderCount { get; set; }
+        public string MostRecentOrderDate { get; set; }
+
+        public CustomerSumaryInput Input { get; set; }
+
+        /* [input model] */
+        public class CustomerSumaryInput 
+        {
+            public int Number { get; set; }
+            public bool Active { get; set; }
+        }
+    }
+
+    /* [domain model] */
+    public class Customer 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ServiceLevel { get; set; }
+
+        public List<Order> Orders { get; set; }
+    }
+
+    /* [domain model] */
+    public class Order 
+    {
+        public int Id { get; set; }
+        public DateTime OrderTime { get; set; }
+    }
+}
