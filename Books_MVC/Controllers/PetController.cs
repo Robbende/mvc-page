8e1f00f4dc2ee30fb50b6e406cba89cc11ea3d56
Ruben Deambrossi <ruben.deambrossi@foxconn.com>
2018-05-29 16:31:06 -0600

    RedirectToAction

---
+++ Books_MVC/Controllers/PetController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Books_MVC.Models;
+
+namespace Books_MVC.Controllers
+{
+    public class PetController : Controller
+    {
+        public static List<Pet> Pets = new List<Pet>(){
+            new Pet(){id=1, name="firulais", ownerId=10},
+            new Pet(){id=2, name="boby", ownerId=10},
+            new Pet(){id=3, name="colitas", ownerId=10}
+        };
+
+
+        //
+        // GET: /Pet/
+
+        public ActionResult Index()
+        {
+            var pet_id = (string)RouteData.Values["id"];
+
+            var pet = Pets.Where(i => i.id == int.Parse(pet_id)).FirstOrDefault();
+
+            if (pet == null)
+            {
+                return RedirectToAction("NotFound", new { message = "Pet with the Id: " + pet_id + " was not found" });
+            }
+
+            return View(pet);
+        }
+
+        
+        public ActionResult NotFound(string message)
+        {
+            ViewBag.msg = message;
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Books_MVC/Models/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Books_MVC.Models
+{
+    public class Pet
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int ownerId { get; set; }
+    }
+}
